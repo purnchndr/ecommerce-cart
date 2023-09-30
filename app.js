d6b9f1b37727b58214daff7c93595f0d7556fb21
Purnachandra <pratap.careers@gmail.com>
2023-09-30 12:30:59 +0530

    adding mysql connection

--- app.js
+++ app.js
@@ -1,64 +1,57 @@
 const express = require("express");
 const app = express();
 
-const productList = require("./products");
-const discountRule = require("./discountRule");
+const productList = require("./src/products");
+const discountRule = require("./src/discountRule");
+const cart = require("./src/cart");
 
 app.use(express.json());
-
-const cart = [
-  {
-    id: "A",
-    name: "Apple",
-    price: 30,
-    quantity: 90,
-  },
-
-  {
-    id: "B",
-    name: "Orange",
-    price: 20,
-    quantity: 10,
-  },
-  {
-    id: "C",
-    name: "Grapes",
-    price: 50,
-    quantity: 40,
-  },
-  {
-    id: "D",
-    name: "Mango",
-    price: 15,
-    quantity: 70,
-  },
-];
 
 app.get("/cart", (req, res, next) => {
   const cartTotal = cart.reduce((acc, curr) => acc + curr.price, 0);
   const discounts = discountRule(cart, cartTotal);
   console.log(cart);
   console.log(discounts);
-  res.send({ cartTotal, ...cart });
+
+  const products = cart.map((curr) => {
+    const item = { ...curr };
+    item.discount = discounts[curr.id] ? discounts[curr.id] : 0;
+    return item;
+  });
+
+  const totalDiscount = products.reduce((acc, curr) => acc + curr.discount, 0);
+
+  res.send({ cartTotal, totalDiscount, ...products });
 });
 
 app.post("/cart/add", (req, res, next) => {
   const { item } = req.body;
 
-  if (!item || !item.id || !item.name || !item.price || !item.quantity)
+  if (
+    !item ||
+    !item.id ||
+    !item.quantity ||
+    item.id.charCodeAt(0) < 65 ||
+    item.id.charCodeAt(0) > 68
+  )
     return res.status(400).send({ message: "Invalid Request" });
 
   const product = cart.find((curr) => curr.id === item.id);
 
   if (product) {
     product.quantity = product.quantity + item.quantity;
-    res.status(200).send({
-      message: "Product Already present in cart merging, Quantity",
+    return res.status(200).send({
+      message: "Product Already present in cart, merging Quantity",
       product,
     });
   } else {
-    cart.push(item);
-    res.status(200).send({ message: "Added to cart", item });
+    const newItem = {
+      quantity: item.quantity,
+      ...productList[item.id.charCodeAt(0) - 65],
+    };
+    cart.push(newItem);
+    console.log(cart);
+    res.status(200).send({ message: "Added to cart", newItem });
   }
 });
 
